ececff5b27619d3f76495cda68f89a961589bd1c
em <eugen.mayer@kontextwork.de>
2016-12-28 09:41:29 +0100

    add automation notice

--- create_production_env.sh
+++ create_production_env.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 cp -fr example/* production
-echo "TODO: generate new NetworkKey for open-zwave for production/configuration/openzwave/options.xml"
+echo "TODO automation: generate new NetworkKey for open-zwave for production/configuration/openzwave/options.xml"
+echo "TODO automation: change container_name in docker-compose.ym from hass_example to hass"
 
 echo "NOTICE: be sure you configure at least production/configuration/hass/custom/homeassistant.yaml - add your timezone and so on"
 echo "Configuration created"
