8c14c4bfa0206be6a246372171b486297e76e597
em <eugen.mayer@kontextwork.de>
2016-12-28 10:33:49 +0100

    add stop

---
+++ stop.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ -d production/configuration ]; then
+	echo "Found production configuration, starting production"
+	cd example/docker
+else
+	echo "Only found example configuration, starting example"
+	cd example/docker
+fi
+
+docker-compose stop
